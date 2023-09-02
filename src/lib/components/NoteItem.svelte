73297015826fd42e45d6c7718686f71fa19ab026
Garrett Nelson <gjnels@proton.me>
2023-09-02 17:45:07 -0400

    Move note item padding to anchor tag

--- src/lib/components/NoteItem.svelte
+++ src/lib/components/NoteItem.svelte
@@ -5,9 +5,9 @@ export let note: Note
 </script>
 
 <li
-  class="grid rounded border-2 border-slate-500 bg-slate-200 p-2 transition focus-within:border-orange-500 hover:scale-105 hover:border-orange-500 hover:bg-transparent dark:bg-slate-800 dark:focus-within:border-orange-400 dark:hover:border-orange-400"
+  class="grid rounded border-2 border-slate-500 bg-slate-200 transition focus-within:border-orange-500 hover:scale-105 hover:border-orange-500 hover:bg-transparent dark:bg-slate-800 dark:focus-within:border-orange-400 dark:hover:border-orange-400"
 >
-  <a href="/edit?id={note.id}" class="flex flex-col justify-between gap-2 outline-none">
+  <a href="/edit?id={note.id}" class="flex flex-col justify-between gap-2 p-2 outline-none">
     <p class="text-lg font-medium leading-tight">{note.title}</p>
     <p class="text-sm opacity-70">{note.updatedAt.toLocaleDateString()}</p>
   </a>
