39ed62c41fd69594f462634ae8a1443d97ef9797
Garrett Nelson <gjnels@proton.me>
2023-09-02 14:55:04 -0400

    Remove unused code

--- src/lib/components/NoteForm.svelte
+++ src/lib/components/NoteForm.svelte
@@ -1,21 +1,13 @@
 <script lang="ts">
 import { twMerge } from 'tailwind-merge'
-import { noteToUpdate, notes } from '$lib/stores/notesStore'
-import { redirect } from '@sveltejs/kit'
 import { goto } from '$app/navigation'
+import { notes } from '$lib/stores/notesStore'
 
 export let classes = ''
 
 export let title = ''
 export let body = ''
 export let id = ''
-
-function handleResetForm() {
-  $noteToUpdate = undefined
-  title = ''
-  body = ''
-  id = ''
-}
 
 function handleSubmit() {
   if (!title) return
@@ -34,17 +26,17 @@ function handleSubmit() {
     type="text"
     placeholder="Title"
     bind:value={title}
-    class="rounded border-2 border-slate-400 bg-transparent p-2 outline-none transition focus-visible:border-slate-900 dark:border-slate-500 dark:focus-visible:border-slate-50"
+    class="rounded border-2 border-slate-400 bg-slate-50 p-2 outline-none transition focus-visible:border-slate-900 dark:border-slate-500 dark:bg-slate-800 dark:focus-visible:border-slate-50"
   />
   <textarea
     rows={5}
     placeholder="Body"
     bind:value={body}
-    class="rounded border-2 border-slate-400 bg-transparent p-2 outline-none transition focus-visible:border-slate-900 dark:border-slate-500 dark:focus-visible:border-slate-50"
+    class="rounded border-2 border-slate-400 bg-slate-50 p-2 outline-none transition focus-visible:border-slate-900 dark:border-slate-500 dark:bg-slate-800 dark:focus-visible:border-slate-50"
   />
   <button
     type="submit"
-    class="rounded bg-orange-400 px-3 py-1.5 font-semibold text-black outline-none transition hover:brightness-110 dark:bg-orange-500 dark:text-white"
+    class="rounded bg-orange-400 px-3 py-1.5 font-semibold outline-none transition hover:brightness-110 dark:bg-orange-500"
     >{id ? 'Update' : 'Create'} Note</button
   >
 </form>
