cc78261815b10108c2e7b4949c289a590a53932d
Garrett Nelson <gjnels@proton.me>
2023-09-02 18:18:30 -0400

    Change navbar color

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -3,7 +3,7 @@ import Logo from './Logo.svelte'
 import Navlink from './Navlink.svelte'
 </script>
 
-<header class="flex justify-between gap-4 bg-slate-200 px-4 py-2 dark:bg-slate-800">
+<header class="flex justify-between gap-4 bg-slate-300 px-4 py-2 dark:bg-slate-700">
   <Logo />
   <nav>
     <Navlink href="/create">New Note</Navlink>
