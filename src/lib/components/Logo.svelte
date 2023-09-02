543652c6a70a0f99fef84f5ad8d26d9a238214ed
Garrett Nelson <gjnels@proton.me>
2023-09-02 17:43:51 -0400

    Move logo to separate component

---
+++ src/lib/components/Logo.svelte
@@ -0,0 +1,5 @@
+<h1
+  class="text-xl font-bold transition focus-within:text-orange-500 hover:text-orange-500 dark:focus-within:text-orange-400 dark:hover:text-orange-400"
+>
+  <a href="/" class="outline-none">Notation</a>
+</h1>
--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,11 +1,10 @@
 <script lang="ts">
+import Logo from './Logo.svelte'
 import Navlink from './Navlink.svelte'
 </script>
 
 <header class="flex justify-between gap-4 bg-slate-200 px-4 py-2 dark:bg-slate-800">
-  <h1 class="text-xl font-bold">
-    <a href="/">Notation</a>
-  </h1>
+  <Logo />
   <nav>
     <Navlink href="/create">New Note</Navlink>
   </nav>
