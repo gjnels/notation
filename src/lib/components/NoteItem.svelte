658f08592d3a478c8aabc8897b1eaefc4bfa78c3
Garrett Nelson <gjnels@proton.me>
2023-09-02 17:43:41 -0400

    Lighten note item border color for dark mode

--- src/lib/components/NoteItem.svelte
+++ src/lib/components/NoteItem.svelte
@@ -5,7 +5,7 @@ export let note: Note
 </script>
 
 <li
-  class="grid rounded border-2 border-slate-500 bg-slate-200 p-2 transition focus-within:border-orange-500 hover:scale-105 hover:border-orange-500 hover:bg-transparent dark:bg-slate-800"
+  class="grid rounded border-2 border-slate-500 bg-slate-200 p-2 transition focus-within:border-orange-500 hover:scale-105 hover:border-orange-500 hover:bg-transparent dark:bg-slate-800 dark:focus-within:border-orange-400 dark:hover:border-orange-400"
 >
   <a href="/edit?id={note.id}" class="flex flex-col justify-between gap-2 outline-none">
     <p class="text-lg font-medium leading-tight">{note.title}</p>
