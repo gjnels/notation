75b7dafb3598fc9c8ebeb459fc1f11162cb0517d
Garrett Nelson <gjnels@proton.me>
2023-09-02 17:38:16 -0400

    Add more styling to note list

--- src/lib/components/NoteItem.svelte
+++ src/lib/components/NoteItem.svelte
@@ -5,10 +5,10 @@ export let note: Note
 </script>
 
 <li
-  class="rounded border-2 border-slate-500 p-2 transition hover:scale-105 hover:border-slate-900 dark:hover:border-slate-50"
+  class="grid rounded border-2 border-slate-500 bg-slate-200 p-2 transition focus-within:border-orange-500 hover:scale-105 hover:border-orange-500 hover:bg-transparent dark:bg-slate-800"
 >
-  <a href="/edit?id={note.id}">
-    <p class="text-lg font-medium">{note.title}</p>
+  <a href="/edit?id={note.id}" class="flex flex-col justify-between gap-2 outline-none">
+    <p class="text-lg font-medium leading-tight">{note.title}</p>
     <p class="text-sm opacity-70">{note.updatedAt.toLocaleDateString()}</p>
   </a>
 </li>
--- src/lib/components/NotesList.svelte
+++ src/lib/components/NotesList.svelte
@@ -3,7 +3,7 @@ import { notes } from '$lib/stores/notesStore'
 import NoteItem from './NoteItem.svelte'
 </script>
 
-<ul class="grid grid-cols-[repeat(auto-fill,_minmax(15rem,_1fr))] gap-4">
+<ul class="grid grid-cols-[repeat(auto-fill,_minmax(12rem,_1fr))] gap-4">
   {#each $notes as note (note.id)}
     <NoteItem note={note} />
   {/each}
