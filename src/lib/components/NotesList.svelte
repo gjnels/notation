1308ed9526a9c583348c9b12da4b99b123a43b06
Garrett Nelson <gjnels@proton.me>
2023-09-02 15:37:15 -0400

    Move notes list to component

---
+++ src/lib/components/NotesList.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+import { notes } from '$lib/stores/notesStore'
+</script>
+
+<ul class="space-y-3">
+  {#each $notes as note (note.id)}
+    <li>
+      <a href="/edit?id={note.id}">
+        <p class="text-lg font-medium">{note.title}</p>
+        <p class="text-sm opacity-70">{note.updatedAt.toLocaleDateString()}</p>
+      </a>
+    </li>
+  {/each}
+</ul>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,5 @@
 <script lang="ts">
-import { notes } from '$lib/stores/notesStore'
+import NotesList from '$lib/components/NotesList.svelte'
 </script>
 
-<a
-  href="/create"
-  class="mb-4 inline-block rounded bg-orange-400 px-3 py-1.5 text-sm font-semibold text-black outline-none transition hover:brightness-110 dark:bg-orange-500 dark:text-white"
-  >New Note</a
->
-
-<ul class="space-y-3">
-  {#each $notes as note (note.id)}
-    <li>
-      <a href="/edit?id={note.id}">
-        <p class="text-lg font-medium">{note.title}</p>
-        <p class="text-sm opacity-70">{note.updatedAt.toLocaleDateString()}</p>
-      </a>
-    </li>
-  {/each}
-</ul>
+<NotesList />
