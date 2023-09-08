8ba37015fd662bac117f1282dc08a90ad80623ab
Garrett Nelson <gjnels@proton.me>
2023-09-08 13:55:24 -0400

    Protect app when unauthenticated

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -2,8 +2,9 @@ import type { LayoutLoad } from './$types'
 import type { Database } from '../database.types'
 import { createSupabaseLoadClient } from '@supabase/auth-helpers-sveltekit'
 import { PUBLIC_SUPABASE_ANON_KEY, PUBLIC_SUPABASE_URL } from '$env/static/public'
+import { redirect } from '@sveltejs/kit'
 
-export const load: LayoutLoad = async function ({ fetch, data, depends }) {
+export const load: LayoutLoad = async function ({ fetch, data, depends, url }) {
   depends('supabase:auth')
 
   const supabase = createSupabaseLoadClient<Database>({
@@ -16,6 +17,9 @@ export const load: LayoutLoad = async function ({ fetch, data, depends }) {
   const {
     data: { session }
   } = await supabase.auth.getSession()
+
+  // protect entire application if not authenticated
+  if (!session && url.pathname !== '/login') throw redirect(303, '/login')
 
   return { supabase, session }
 }
