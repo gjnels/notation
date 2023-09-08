7bdafc9ce411ac3c5f85d48fe7cbaf862935f1be
Garrett Nelson <gjnels@proton.me>
2023-09-08 18:21:07 -0400

    Fix conflicts with global link styles

--- src/lib/components/Logo.svelte
+++ src/lib/components/Logo.svelte
@@ -1,5 +1,5 @@
 <h1
   class="text-xl font-bold transition focus-within:text-primary-500 hover:text-primary-500 dark:focus-within:text-primary-400 dark:hover:text-primary-400"
 >
-  <a href="/" class="outline-none">Notation</a>
+  <a href="/" class="unstyled outline-none">Notation</a>
 </h1>
--- src/lib/components/Navlink.svelte
+++ src/lib/components/Navlink.svelte
@@ -10,7 +10,7 @@
 <a
   {href}
   class={twMerge(
-    'border-b-2 border-transparent py-0.5 text-sm font-medium outline-none hover:text-primary-500 focus-visible:text-primary-500 dark:hover:text-primary-400 dark:focus-visible:text-primary-400',
+    'unstyled border-b-2 border-transparent py-0.5 text-sm font-medium outline-none hover:text-primary-500 focus-visible:text-primary-500 dark:hover:text-primary-400 dark:focus-visible:text-primary-400',
     active && 'border-primary-500 opacity-100'
   )}
 >
